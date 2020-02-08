31b88e7816281251b1c5d2890640bb3ec9cb9548
Luke <luke@lukehanslarsen.com>
2020-02-07 19:30:38 -0500

    Update Feb7
    Auto

--- Auto.php
+++ Auto.php
@@ -1,8 +1,48 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_once ('Php/config.php');
 require_once ('Php/mysqli.php');
 if($EnableAutoFaucet != true){
   header('location:404.php');
+}
+
+//SORTLINKSTUFF
+if(isset($_GET['id']) &&!empty($_GET['id'])){
+	$ShortlinkCheck = $_GET['id'];
+}
+if(isset($ShortlinkCheck) &&!empty($ShortlinkCheck)){
+    $ShortLinkCachCheck = $_SESSION['ShortLinkCheck'];
+    $id = $_SESSION['ShortLinkid'];
+	if($ShortlinkCheck == $ShortLinkCachCheck){
+            echo "Success!<br>";
+            $GiveReward = mysqli_query($con,"UPDATE `User` SET `Tokens`=`Tokens`+1 WHERE `UserID` = $User");
+            $LogClaim = mysqli_query($con,"INSERT INTO `ShortLinkClaims`(`UserID`, `ShortlinkId`, `Time`) VALUES ('$User','$id','$curtime')");
+            $AddingATimeClaimed = mysqli_query($con, "UPDATE `ShortLinks` SET `TimesClicked`=`TimesClicked`+1 WHERE `ShortlinkId` = $id");
+            $_SESSION['ShortLinkCheck'] = '';
+            $_SESSION['ShortLinkid'] = '';
+	}
+}
+
+if(isset($_POST['submit'])){
+    $id = filter_var($_POST['id'], FILTER_SANITIZE_NUMBER_INT);
+    $shortlinksS = mysqli_query($con, "SELECT * FROM `ShortLinks` WHERE ShortlinkId=$id");
+    While($ShortlinksFetchedPush = mysqli_fetch_assoc($shortlinksS)){
+        $RandomNumberForCheck = rand(1,100);
+        $APILink = $ShortlinksFetchedPush['APILink'];
+        $API = $ShortlinksFetchedPush['API'];
+        $_SESSION['ShortLinkCheck'] = $RandomNumberForCheck;
+        $_SESSION['ShortLinkid'] = $ShortlinksFetchedPush['ShortlinkId'];;
+        //$url = $APILink .'?api='. $API . "&url=$HTTP://$URL/Auto.php?id=" . $RandomNumberForCheck . '&format=text';
+        $url = $APILink .'?api='. $API . "&url=http://www.lukehanslarsen.com/EbitsFaucetR/Auto.php?id=" . $RandomNumberForCheck . '&format=text';
+        $ch = curl_init();
+        curl_setopt($ch,CURLOPT_URL,$url);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+        $output=curl_exec($ch);
+        curl_close($ch);
+        echo "<script>window.location = '$output';</script>";
+    }
 }
 
 ?>
@@ -23,9 +63,45 @@ if($EnableAutoFaucet != true){
         <div>
           <!-- Put banner ads in these places -->
         </div>
-        <div>
-
-        </div>
+          <div>
+            <center>
+              <?php
+              $clause = '';
+              $shortlinkclaims = mysqli_query($con, "SELECT ShortlinkId,Time FROM ShortLinkClaims WHERE UserID = $User");
+              if(isset($shortlinkclaims)&& !empty($shortlinkclaims) &&$shortlinkclaims->num_rows != 0){
+                  while($ShortlinksClaimFetched = mysqli_fetch_assoc($shortlinkclaims)){
+                      $timeClaimed = $ShortlinksClaimFetched['Time'];
+                      if($curtime - $timeClaimed < '86400'){
+                          $array[] = $ShortlinksClaimFetched['ShortlinkId'];
+                      }
+                  }
+                  if(!empty($array) && isset($array)){
+                      foreach ($array as $a) {$clause.= "'$a',";}
+                      $clause=substr($clause,0,-1);
+                      if(empty($array)){
+                          $clause = '0';
+                      }
+                  }
+                  if(empty($clause)){
+                      $shortlinks = mysqli_query($con, "SELECT * FROM `ShortLinks`");
+                  }else{
+                      $shortlinks = mysqli_query($con, "SELECT * FROM `ShortLinks` WHERE `id` NOT IN ($clause) ORDER BY `id` ASC");
+                  }
+              }else{
+                 $shortlinks = mysqli_query($con, "SELECT * FROM `ShortLinks`");
+              }
+              if(isset($shortlinks)&&!empty($shortlinks) && $shortlinks->num_rows != 0){
+                  While($ShortlinksFetched = mysqli_fetch_assoc($shortlinks)){
+                      $Company = $ShortlinksFetched['Name'];
+                      $Id = $ShortlinksFetched['ShortlinkId'];
+                      echo "<form method='post' action=''><input name='submit' type='submit' class='button' value='$Company'/> <input type='hidden' name='id' value='$Id'/></form>";
+                  }
+              }else{
+                  echo "You have done all the shortlinks you can for today D:";
+              }
+              ?>
+            </center>
+          </div>
         <div>
 <!-- Put banner ads in these places -->
         </div>
